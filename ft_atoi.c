32da9fbb424b02ec0fc312b702f349fd5a403369
s-ike <shunt_i@yahoo.co.jp>
2020-10-11 16:12:47 +0900

    updated ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,9 +6,29 @@ static int	is_space(char c)
 		|| c == '\f' || c == '\t' || c == '\v');
 }
 
+static int	is_over_long(unsigned long n, int is_max)
+{
+	unsigned long	limit;
+
+	limit = __LONG_MAX__;
+	if (!is_max)
+		limit++;
+	return ((limit < n));
+}
+
+static int	is_over_max_long(unsigned long n)
+{
+	return (is_over_long(n, 1));
+}
+
+static int	is_over_min_long(unsigned long n)
+{
+	return (is_over_long(n, 0));
+}
+
 int			ft_atoi(const char *str)
 {
-	unsigned int	num;
+	unsigned long	num;
 	int				sign;
 
 	num = 0;
@@ -25,6 +45,10 @@ int			ft_atoi(const char *str)
 		if (num)
 			num *= 10;
 		num += (*str++ - '0');
+		if (0 < sign && is_over_max_long(num))
+			return ((int)__LONG_MAX__);
+		if (sign < 0 && is_over_min_long(num))
+			return ((int)(-__LONG_MAX__ - 1));
 	}
-	return (sign * num);
+	return (num *= sign);
 }
