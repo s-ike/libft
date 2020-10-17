90c6e8838d39ac4fd70b4f194a0d8c8ca5f1ff35
s-ike <shunt_i@yahoo.co.jp>
2020-10-17 12:21:15 +0900

    added ft_lstclear

---
+++ ft_lstclear.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*tmp;
+
+	if (!lst || !del)
+		return ;
+	while (*lst)
+	{
+		tmp = *lst;
+		*lst = (*lst)->next;
+		ft_lstdelone(tmp, del);
+	}
+}
