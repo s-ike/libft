1dcc571008aeb3ab630ddb6c7d11b770bd0d9fd3
s-ike <shunt_i@yahoo.co.jp>
2020-10-19 23:20:52 +0900

    updated ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -4,7 +4,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	if (n == 0)
 		return (0);
-	while ((*s1 || *s2) && n--)
+	while ((*s1 || *s2) && 0 < n--)
 	{
 		if (*s1 != *s2)
 			return (*(unsigned char *)s1 - *(unsigned char *)s2);
