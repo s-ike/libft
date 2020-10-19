a633038db59e044e5d288cfcf03aaebcc3f19efb
s-ike <shunt_i@yahoo.co.jp>
2020-10-20 00:07:10 +0900

    updated ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -5,7 +5,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*sub;
 	size_t	s_len;
 
-	if (!s || (s_len = ft_strlen(s)) <= start)
+	if (!s)
+		return (NULL);
+	if ((s_len = ft_strlen(s)) <= start || !len)
 		return (ft_calloc(1, sizeof(char)));
 	if (s_len < len + start)
 		len = s_len - start;
