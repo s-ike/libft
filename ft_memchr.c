91ad415a3bd28e9b14e13193c90cdbea0cb70f1b
s-ike <shunt_i@yahoo.co.jp>
2020-10-19 22:38:12 +0900

    updated ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -5,7 +5,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	const unsigned char	*ptr;
 
 	ptr = s;
-	while (n--)
+	while (0 < n--)
 		if (*ptr++ == (unsigned char)c)
 			return ((void *)(ptr - 1));
 	return (NULL);
