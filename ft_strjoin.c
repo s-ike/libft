152ac59bf00dac0abd061d3b71e7b14d4ea417bd
s-ike <shunt_i@yahoo.co.jp>
2020-10-20 17:34:03 +0900

    updated ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,13 +6,17 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s1_len;
 	size_t	s2_len;
 
-	s1_len = !s1 ? 0 : ft_strlen(s1);
-	s2_len = !s2 ? 0 : ft_strlen(s2);
+	if (!s1 && !s2)
+		return (ft_strdup(""));
+	else if (!s1)
+		return (ft_strdup(s2));
+	else if (!s2)
+		return (ft_strdup(s1));
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
 	if (!(str = (char *)malloc((s1_len + s2_len + 1) * sizeof(char))))
 		return (NULL);
-	if (s1)
-		ft_memcpy(str, s1, s1_len + 1);
-	if (s2)
-		ft_memcpy(str + s1_len, s2, s2_len + 1);
+	ft_memcpy(str, s1, s1_len + 1);
+	ft_memcpy(str + s1_len, s2, s2_len + 1);
 	return (str);
 }
