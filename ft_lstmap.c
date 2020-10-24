db9599e3b3cfdff8655a97da942e2a48e7c6b532
s-ike <shunt_i@yahoo.co.jp>
2020-10-24 14:02:32 +0900

    updated ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -5,7 +5,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new;
 	t_list	*new_head;
 
-	if (!lst || !f)
+	if (!lst || !f || !del)
 		return (NULL);
 	if (!(new = ft_lstnew(f(lst->content))))
 		return (NULL);
