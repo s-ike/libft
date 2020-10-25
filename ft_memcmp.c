4bf2bb0268e577d5226891a8e4e1f3df5008fef3
s-ike <shunt_i@yahoo.co.jp>
2020-10-25 10:50:46 +0900

    updated ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -7,10 +7,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	c1 = s1;
 	c2 = s2;
-	while (n--)
-	{
+	while (0 < n--)
 		if (*c1++ != *c2++)
 			return (*(c1 - 1) - *(c2 - 1));
-	}
 	return (0);
 }
