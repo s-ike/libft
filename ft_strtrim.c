289012018971d3ba241defba6da8450faf72eb20
s-ike <shunt_i@yahoo.co.jp>
2021-07-07 23:47:35 +0900

    norm ft_strtrim & ft_substr

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -7,12 +7,15 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	if (!s1)
 		return (NULL);
-	while (set && *s1 && ft_strchr(set, *s1))
+	if (!set)
+		return (ft_strdup(s1));
+	while (*s1 && ft_strchr(set, *s1))
 		s1++;
 	len = ft_strlen(s1);
-	while (set && 0 < len && ft_strchr(set, s1[len - 1]))
+	while (0 < len && ft_strchr(set, s1[len - 1]))
 		len--;
-	if (!(str = (char *)malloc((len + 1) * sizeof(char))))
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
 		return (NULL);
 	ft_strlcpy(str, s1, len + 1);
 	return (str);
--- ft_substr.c
+++ ft_substr.c
@@ -7,10 +7,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s)
 		return (NULL);
-	if ((s_len = ft_strlen(s)) <= start || len < 1)
+	s_len = ft_strlen(s);
+	if (s_len <= start || len < 1)
 		return ((char *)ft_calloc(1, sizeof(char)));
-	len = (s_len < len) ? (s_len - start) : len;
-	if (!(sub = (char *)malloc((len + 1) * sizeof(char))))
+	if (s_len < len)
+		len = s_len - start;
+	sub = (char *)malloc((len + 1) * sizeof(char));
+	if (!sub)
 		return (NULL);
 	s += start;
 	ft_strlcpy(sub, s, len + 1);
