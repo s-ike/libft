05fb18f52fde3c5caf77c6419976483c5090691e
s-ike <shunt_i@yahoo.co.jp>
2021-07-07 23:07:01 +0900

    update ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,30 +6,10 @@ static int	is_space(char c)
 		|| c == '\f' || c == '\t' || c == '\v');
 }
 
-static int	is_over_long(unsigned long n, int is_max)
+long	ft_atol(const char *str)
 {
-	unsigned long	limit;
-
-	limit = __LONG_MAX__;
-	if (!is_max)
-		limit++;
-	return ((limit < n));
-}
-
-static int	is_over_max_long(unsigned long n)
-{
-	return (is_over_long(n, 1));
-}
-
-static int	is_over_min_long(unsigned long n)
-{
-	return (is_over_long(n, 0));
-}
-
-long		ft_atol(const char *str)
-{
-	unsigned long	num;
-	int				sign;
+	unsigned long long	num;
+	int					sign;
 
 	num = 0;
 	sign = 1;
@@ -37,21 +17,24 @@ long		ft_atol(const char *str)
 		str++;
 	if (*str == '-' || *str == '+')
 	{
-		sign = *str == '-' ? -1 : 1;
+		if (*str == '-')
+			sign = -1;
 		str++;
 	}
-	while (*str && ft_isdigit(*str))
+	while (ft_isdigit(*str))
 	{
 		num = (num * 10) + (*str++ - '0');
-		if (0 < sign && is_over_max_long(num))
+		if (num >> 63)
+		{
+			if (sign == -1)
+				return (-__LONG_MAX__ - 1);
 			return (__LONG_MAX__);
-		if (sign < 0 && is_over_min_long(num))
-			return (-__LONG_MAX__ - 1);
+		}
 	}
 	return (num * sign);
 }
 
-int			ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
 	return ((int)ft_atol(str));
 }
